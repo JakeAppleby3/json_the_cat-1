ff8d3d8780d3c4f623c553bfe0fe8a218a166aae
Jake Appleby <jakeappleby@live.com>
2021-07-01 22:32:56 +0000

    BreedGetter function.

--- breedGetter.js
+++ breedGetter.js
@@ -1,23 +1,24 @@
 const request = require('request');
-const catName = process.argv[2];
-request(`https://api.thecatapi.com/v1/breeds/search?q=${catName}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode);
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (response.statusCode > 299) {
-    console.log("This is status code error:", error);
-    return;
-  }
-  const data = JSON.parse(body);
-  console.log(data);
-  if (data.length === 0) {
-    console.log("This cat breed does not exist!");
-    return;
-  }
-  console.log(typeof data);
-  console.log(data[0].description);
-  console.log(process.argv);
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode > 299) {
+      callback('This is a status code error', null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback("This cat breed does not exist!", null);
+      return;
+    }
+    callback(null, data[0].description);
+  });
+};
+
+
+
+module.exports = { fetchBreedDescription };
